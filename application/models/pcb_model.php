589734619335b7bd3899ae85c32611315f476193
Hernan Sarmiento <hernan.sarmientoa@gmail.com>
2013-09-23 15:59:24 -0300

    Se cambia limite de graficos y se agrega adicion de hora para produccion

--- application/models/pcb_model.php
+++ application/models/pcb_model.php
@@ -26,7 +26,7 @@ class Pcb_model extends CI_Model {
                 as sensor_identifier, temperature.value as t_value, temperature.created_at as created_at 
                 FROM (pcb) LEFT JOIN sensors ON pcb.id = sensors.pcb_id 
                 JOIN temperature ON sensors.id = temperature.sensor_id WHERE pcb.id = '.$pcb_id.' 
-                and temperature.created_at >= NOW() - INTERVAL 2 DAY 
+                and temperature.created_at >= NOW() - INTERVAL 1 DAY 
                 ORDER BY sensors.id asc, temperature.created_at asc'
             );
         return $query->result_array();
@@ -44,18 +44,24 @@ class Pcb_model extends CI_Model {
 
     public function get_sensor_heater_with_pcb($pcb_id)
     {
-        $this->db->select('t1.id as pcb_id, t1.identifier as pcb_identifer, 
-                            t2.id as sensor_id, t2.identifier as sensor_identifier,
-                            t3.status as status, t3.created_at as created_at,
-                            t3.stopped_at as stopped_at, t4.value as temperature')
-        ->from('pcb as t1')
-        ->join('sensors as t2', 't1.id = t2.pcb_id')
-        ->join('heaters as t3', 't2.id = t3.sensor_id')
-        ->join('temperature as t4', 't2.id = t4.sensor_id','left')
-        ->where('t1.id',$pcb_id)
-        ->order_by('t3.id','desc')
-        ->limit(1);
-        return $this->db->get()->row_array();
+        // $query = $this->db->query('select t1.id as pcb_id, t1.identifier as pcb_identifer, 
+        //     t2.id as sensor_id, t2.identifier as sensor_identifier,
+        //     t3.status as status, date_add(t3.created_at, interval 2 hour) as created_at,
+        //     date_add(t3.stopped_at , interval 2 hour) as stopped_at, t4.value as temperature from pcb as t1 join 
+        //     sensors as t2 on t1.id = t2.pcb_id 
+        //     join heaters as t3 on t2.id = t3.sensor_id 
+        //     left join temperature as t4 on t2.id = t4.sensor_id where t1.id = '.$pcb_id.'
+        //     order by t3.id desc limit 1;');
+
+        $query = $this->db->query('select t1.id as pcb_id, t1.identifier as pcb_identifer, 
+            t2.id as sensor_id, t2.identifier as sensor_identifier,
+            t3.status as status, t3.created_at as created_at,
+            t3.stopped_at as stopped_at, t4.value as temperature from pcb as t1 join 
+            sensors as t2 on t1.id = t2.pcb_id 
+            join heaters as t3 on t2.id = t3.sensor_id 
+            left join temperature as t4 on t2.id = t4.sensor_id where t1.id = '.$pcb_id.'
+            order by t3.id desc limit 1;');
+        return $query->row_array();
     }
 
 }
